657c30ad3fd5b389f08c01e4e87cf46ddf5009f0
JerryLiao26 <jerryliao26@gmail.com>
2018-02-17 10:04:51 +0800

    redefine structure

--- grenade.js
+++ grenade.js
@@ -1,57 +1,29 @@
-// Log level
-let I = "Info"
-let W = "Warning"
-let E = "Error"
+let G = {
+  // Log level
+  I: "Info",
+  W: "Warning",
+  E: "Error",
 
-// Logger
-let gLogger = function(level, content) {
-  // Build string
-  let thisDate = new Date();
-  let prefix = ""
-  let timeStr = "[" + thisDate.toLocaleString() + "]"
-  let levelStr = "[" + level + "]"
-  if (level === I) {
-    console.info(timeStr, levelStr, content)
-  }
-  else if (level === W) {
-    console.warn(timeStr, levelStr, content)
-  }
-  else if (level === E) {
-    console.error(timeStr, levelStr, content)
-  }
-}
+  // Logger
+  gLogger: function(level, content) {
+    // Build string
+    let thisDate = new Date();
+    let prefix = ""
+    let timeStr = "[" + thisDate.toLocaleString() + "]"
+    let levelStr = "[" + level + "]"
+    if (level === I) {
+      console.info(timeStr, levelStr, content)
+    }
+    else if (level === W) {
+      console.warn(timeStr, levelStr, content)
+    }
+    else if (level === E) {
+      console.error(timeStr, levelStr, content)
+    }
+  },
 
-// Init grenade object
-let G = function(selector) {
-  if (typeof selector == "string" ) {
-    let group = document.querySelectorAll(selector)
-    // Check selected nodes
-    if (group.length >= 1) {
-      console.log('selected:', group);
-      let gt = new gTarget(group)
-      return gt
-    }
-    else {
-      gLogger(E, "no target selected")
-    }
-  }
-  else {
-    gLogger(E, "wrong type of param for G()")
-  }
-}
-
-//
-class gTarget {
-  constructor(nodeList) {
-    if (nodeList instanceof NodeList) {
-      this.target = nodeList
-    }
-    else {
-      gLogger(E, "cannot aim at wrong type of target(s)")
-    }
-  }
   // Pack data
-  pack(first, ...others) {
+  pack: function(first, ...others) {
     let data = []
     if (first !== undefined) {
       data.push(first)
@@ -59,7 +31,7 @@ class gTarget {
         data.push(each)
       }
       console.log('data:', data);
-      let gr = new gReady(data)
+      let gr = new grenade(data)
       return gr
     }
     else {
@@ -68,8 +40,8 @@ class gTarget {
   }
 }
 
-//
-class gReady {
+// Class grenade
+class grenade {
   constructor(data) {
     if (Array.isArray(data)) {
       this.data = data
@@ -78,8 +50,20 @@ class gReady {
       gLogger(E, "cannot pack fault material(s) in grenade")
     }
   }
-  // Throw(Render) data
-  throw(data) {
-    console.log('Thrown!');
+  // Select target
+  throw(selector) {
+    if (typeof selector == "string" ) {
+      let group = document.querySelectorAll(selector)
+      // Check selected nodes
+      if (group.length >= 1) {
+        console.log('selected:', group);
+      }
+      else {
+        gLogger(E, "no target selected")
+      }
+    }
+    else {
+      gLogger(E, "wrong type of param for G()")
+    }
   }
 }
