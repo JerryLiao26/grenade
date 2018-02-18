cc2327ba104c78461c14c29b4fe8cb2c28954998
JerryLiao26 <jerryliao26@gmail.com>
2018-02-18 21:15:09 +0800

    add render loop function

--- grenade.js
+++ grenade.js
@@ -74,18 +74,53 @@ class grenade {
     }
   }
   // Render data
-  throw(data) {
+  throw(data, loop=false) {
     if (data !== undefined) {
       // Array render
       if (Array.isArray(data)) {
-        for (let eachNode of this.list) {
-          // Filter nodes
-          let count = -1
-          let len = data.length
-          let gNodes = eachNode.querySelectorAll('.g-node')
-          for (let eachG of gNodes) {
-            count = (count + 1) % len
-            this.renderer(eachG, data[count])
+        if (loop === false) {
+          for (let eachNode of this.list) {
+            // Filter nodes
+            let count = -1
+            let len = data.length
+            let gNodes = eachNode.querySelectorAll('.g-node')
+            for (let eachG of gNodes) {
+              count = (count + 1) % len
+              this.renderer(eachG, data[count])
+            }
+          }
+        }
+        // List of objects
+        else {
+          for (let eachNode of this.list) {
+            // Init for loop
+            let count = 0
+            let prevNode = null
+            let curNode = eachNode
+            for (let eachDatum of data) {
+              // Clone node
+              let newNode = curNode.cloneNode(true)
+              // If first entrance, do not insert
+              if (count !== data.length-1) {
+                curNode.parentElement.insertBefore(newNode, curNode.nextSibling)
+              }
+              // First node
+              let gNodes = curNode.querySelectorAll('.g-node')
+              for (let eachG of gNodes) {
+                let key = eachG.getAttribute('g-key')
+                if (key === "$index") {
+                  this.renderer(eachG, count+1)
+                }
+                else {
+                  this.renderer(eachG, eachDatum[key])
+                }
+              }
+              // Switch node
+              prevNode = curNode
+              curNode = newNode
+              // Counter increment
+              count++
+            }
           }
         }
       }
